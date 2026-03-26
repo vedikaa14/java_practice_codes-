a4304f47399cbc388c0920f0f0ad24ffa6420482
Vedika pailwan <vedikapailwan44@gmail.com>
2026-03-27 00:37:54 +0530

    initial program

---
+++ Program1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Program1 {
+     public static void main(String[] args) {
+         Scanner input =new Scanner(System.in);
+         System.out.println("enter integer:");
+        int  n = input.nextInt();
+         System.out.println("enter double:");
+         double  d = input.nextDouble();
+         System.out.println("Integer input is " + n);
+         System.out.println("double input is " + d);
+    }
+       
+        }
